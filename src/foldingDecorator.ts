82b716f4afaa11e86d6ffedf0f213eb0a9590ad3
Mohammad Baqer <mtbaqer@gmail.com>
2023-01-13 14:09:48 -0500

    make collapsedText clickable to unfold

--- src/foldingDecorator.ts
+++ src/foldingDecorator.ts
@@ -75,6 +75,9 @@ export default class FoldingDecorator {
     return {
       textDecoration: "none; display:none;", //Hides the folded text
       before: {
+        //Apparently if you add width and height (any values), the text will be clickable
+        width: "0",
+        height: "0",
         contentText,
         color: "rgba(255, 255, 255, 0.5)", //TODO: Get this from the theme
         margin: `0 -${100}% 0 0`, //Hides the original collapsed text '…'
