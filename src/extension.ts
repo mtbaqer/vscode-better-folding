c25cedf978ddff43bf3e7040d5d84b54ce826645
Mohammad Baqer <mtbaqer@gmail.com>
2023-01-12 19:23:02 -0500

    add a folding provider that can override the default language provider

--- src/extension.ts
+++ src/extension.ts
@@ -1,7 +1,47 @@
-import { ExtensionContext, window } from "vscode";
+import { ExtensionContext, FoldingRange, FoldingRangeKind, languages, window } from "vscode";
 
 export function activate(context: ExtensionContext) {
-  console.log("activate");
+  // Courtesy of vscode-explicit-fold,
+  // apparently if you delay the folding provider by a second,
+  // it can override the default language folding provider.
+  setTimeout(() => {
+    context.subscriptions.push(
+      languages.registerFoldingRangeProvider(
+        { language: "typescript", scheme: "file" },
+        {
+          provideFoldingRanges(document) {
+            const ranges = [];
+
+            //regex to match functions in typescript
+            const functionRegex =
+              /function\s*([A-z0-9]+)?\s*\((?:[^)(]+|\((?:[^)(]+|\([^)(]*\))*\))*\)\s*\{(?:[^}{]+|\{(?:[^}{]+|\{[^}{]*\})*\})*\}/g;
+
+            let match;
+            while ((match = functionRegex.exec(document.getText()))) {
+              if (match && !match[0]) continue;
+
+              const startPosition = document.positionAt(match.index);
+              const endPosition = document.positionAt(match.index + match[0].length);
+
+              if (startPosition.line !== endPosition.line) {
+                ranges.push(new FoldingRange(startPosition.line, endPosition.line, FoldingRangeKind.Region));
+              }
+            }
+
+            return ranges;
+          },
+        }
+      )
+    );
+  }, 1000);
+
+  context.subscriptions.push(
+    window.onDidChangeTextEditorVisibleRanges((e) => {
+      if (e) {
+        console.log("onDidChangeTextEditorVisibleRanges");
+      }
+    })
+  );
 }
 
 export function deactivate() {}
