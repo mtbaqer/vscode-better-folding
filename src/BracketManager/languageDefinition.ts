2b2f4e73ce3c03f0fff8f2be78386ec249320fb3
Mohammad Baqer <mtbaqer@gmail.com>
2023-01-16 21:22:55 -0500

    remove bracket pair colorizer's LanguageDefinition

--- src/BracketManager/languageDefinition.ts
+++
@@ -1,7 +0,0 @@
-import ScopePair from "./scopePair";
-
-export default class LanguageDefinition {
-  public readonly language: string;
-  public readonly extends?: string;
-  public readonly scopes?: ScopePair[];
-}
